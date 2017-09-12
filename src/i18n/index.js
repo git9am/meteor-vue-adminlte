cf5b2cd90b013d22969f33c4de0ca2c9f4ac6768
rookiepang <904511044@qq.com>
2017-09-12 14:50:16 +0800

    change lanuage

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,22 +4,26 @@ import VueI18n from 'vue-i18n';
 Vue.use(VueI18n);
 
 export default function i18n(messages) {
+  let lang;
+  const storage = window.localStorage;
+  const index = storage.getItem('indexLang');
+  if (index) {
+    if (index === '0') {
+      lang = 'en';
+      // Vue.config.lang = 'en';
+    }
+    if (index === '1') {
+      lang = 'cn';
+      // Vue.config.lang = 'cn';
+    }
+  } else {
+    lang = 'cn';
+    // Vue.config.lang = 'cn';
+  }
+  Session.set('lang', index);
+
   return new VueI18n({
-    locale: 'cn',
+    locale: lang,
     messages,
   });
 };
-
-const storage = window.localStorage;
-const index = storage.getItem('indexLang');
-if (index) {
-  if (index === '0') {
-    Vue.config.lang = 'en';
-  }
-  if (index === '1') {
-    Vue.config.lang = 'cn';
-  }
-} else {
-  Vue.config.lang = 'cn';
-}
-Session.set('lang', index);
